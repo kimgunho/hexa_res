9c259a92b57e6be2ff04b7e8f19ef1e75a29465c
vi <yuyukgh@naver.com>
2023-03-06 01:28:42 +0900

    특허 페이지 업데이트

--- src/pages/license/index.jsx
+++ src/pages/license/index.jsx
@@ -1,13 +1,53 @@
 import background from "../../assets/images/license/subTopBackground.jpg";
 
+import { Layout } from "../../component/layout";
 import { SubTop } from "../../component/global/subTop";
 
-import { Wrapper } from "./style";
+import { Wrapper, List, Box, Img, NoImg, Info, Title, Desc } from "./style";
 
 function License() {
+  const licenseArr = [
+    {
+      title: "특허01",
+      image: null,
+      desc: "Anisotropic Ceramic Membranes from self",
+    },
+    {
+      title: "특허02",
+      image: null,
+      desc: "Anisotropic Ceramic Membranes from self",
+    },
+    {
+      title: "특허03",
+      image: null,
+      desc: "Anisotropic Ceramic Membranes from self",
+    },
+    {
+      title: "특허04",
+      image: null,
+      desc: "Anisotropic Ceramic Membranes from self",
+    },
+  ];
   return (
     <Wrapper>
       <SubTop text="특허" image={background} />
+      <Layout>
+        <List>
+          {licenseArr.map((item, index) => (
+            <Box key={item.title}>
+              {item.image ? (
+                <Img src={item.image} alt="" />
+              ) : (
+                <NoImg>특허이미지 준비중</NoImg>
+              )}
+              <Info>
+                <Title>{item.title}</Title>
+                <Desc>{item.desc}</Desc>
+              </Info>
+            </Box>
+          ))}
+        </List>
+      </Layout>
     </Wrapper>
   );
 }
--- src/pages/license/style.js
+++ src/pages/license/style.js
@@ -1,3 +1,56 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.section``;
+export const Wrapper = styled.section`
+  margin-bottom: 6.25rem;
+`;
+
+export const List = styled.ul`
+  display: flex;
+  gap: 2rem;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+export const Box = styled.li`
+  width: 23%;
+`;
+
+export const Img = styled.img`
+  width: 100%;
+  height: 17.5rem;
+  object-fit: cover;
+`;
+
+export const NoImg = styled.div`
+  width: 100%;
+  height: 17.5rem;
+  background-color: #333;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1rem;
+  color: #fff;
+`;
+
+export const Info = styled.div`
+  background-color: #f0f0f0;
+  padding: 1rem;
+  border-radius: 0 0 1rem 0;
+`;
+
+export const Title = styled.strong`
+  display: block;
+  font-size: 1.25rem;
+  color: #333;
+  font-weight: 600;
+  margin-bottom: 0.5rem;
+  padding-bottom: 0.5rem;
+  border-bottom: 1px solid #ddd;
+`;
+
+export const Desc = styled.p`
+  font-size: 1rem;
+  color: #4a4a4a;
+  line-height: 1.8;
+  font-weight: 400;
+`;
